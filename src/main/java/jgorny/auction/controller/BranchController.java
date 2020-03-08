624884d0daab85db28598325e5fc5333e13ccf4d
jGorny <iceziro@wp.pl>
2020-03-08 16:44:11 +0100

    added branch getter

--- src/main/java/jgorny/auction/controller/BranchController.java
+++ src/main/java/jgorny/auction/controller/BranchController.java
@@ -1,4 +1,20 @@
 package jgorny.auction.controller;
 
+import jgorny.auction.controller.model.GetBranchResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/branches")
 public class BranchController {
+
+    @GetMapping("{id}")
+    public GetBranchResponse getBranch(@PathVariable("id") Long id){
+        return new GetBranchResponse(id,"meat");
+    }
+
+
+
 }
--- src/main/java/jgorny/auction/controller/model/GetBranchResponse.java
+++ src/main/java/jgorny/auction/controller/model/GetBranchResponse.java
@@ -11,6 +11,7 @@ import lombok.*;
 public class GetBranchResponse {
 
     private Long id;
+
     private String name;
 
 }
