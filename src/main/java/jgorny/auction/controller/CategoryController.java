76040fcb4b62b7caccc98be17065c505ed1a33ae
jGorny <iceziro@wp.pl>
2020-03-10 14:11:35 +0100

    added post and put category requests also added to category controller 3 methods put post delete

--- src/main/java/jgorny/auction/controller/CategoryController.java
+++ src/main/java/jgorny/auction/controller/CategoryController.java
@@ -1,9 +1,41 @@
 package jgorny.auction.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jgorny.auction.controller.model.GetCategoriesResponse;
+import jgorny.auction.controller.model.GetCategoryResponse;
+import jgorny.auction.controller.model.PostCategoryRequest;
+import jgorny.auction.controller.model.PutCategoryRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/branches/{id}")
+@RequestMapping("/api/branches/{id}/categories")
 public class CategoryController {
+
+    @GetMapping("{id}")
+    public GetCategoryResponse getCategory(@PathVariable("id") Long id){
+        return new GetCategoryResponse(id, "phones");
+    }
+
+    @GetMapping("")
+    public GetCategoriesResponse getCategories(@PathVariable("id") Long id){
+        return new GetCategoriesResponse(List.of(24l,8l,2l,42l,4l,2l));
+    }
+    @PostMapping("")
+    public void postCategory(@RequestBody PostCategoryRequest category){
+        System.out.println(category);
+    }
+
+    @PutMapping("{id}")
+    public void putBranch(@PathVariable("id") Long id, @RequestBody PutCategoryRequest category){
+        System.out.println(id+" "+category);
+    }
+
+
+    @DeleteMapping("{id}")
+    public void deleteCategory(@PathVariable("id") Long id){
+        System.out.println(id);
+    }
+
+
 }
