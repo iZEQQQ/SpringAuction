7b937244c611305ba75de70fdd470f8091455c53
jgorny <iceziro@wp.pl>
2020-09-23 13:32:38 +0200

    added basket controller

--- src/main/java/jgorny/portal/basket/controller/BasketController.java
+++ src/main/java/jgorny/portal/basket/controller/BasketController.java
@@ -7,6 +7,7 @@ import jgorny.portal.basket.bean.BasketBean;
 import jgorny.portal.basket.controller.model.BasketItem;
 import jgorny.portal.basket.controller.model.GetBasketItemsResponse;
 import jgorny.portal.basket.controller.model.PostBasketItemRequest;
+import jgorny.portal.basket.controller.model.PutBasketItemRequest;
 import jgorny.portal.user.repository.model.User;
 import jgorny.portal.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -63,7 +64,7 @@ public class BasketController {
                 jgorny.portal.basket.bean.model.BasketItem item = new jgorny.portal.basket.bean.model.BasketItem(
                         request.getQuantity(), auction.get());
                 bean.save(item);
-                return ResponseEntity.created(URI.create("http://localhost:8080/api/users/"+login+"/basket/items/"+bean.findAll().size())).build();
+                return ResponseEntity.created(URI.create("http://localhost:8080/api/users/" + login + "/basket/items/" + bean.findAll().size())).build();
             } else {
                 return ResponseEntity.badRequest().build();
             }
@@ -71,5 +72,36 @@ public class BasketController {
             return ResponseEntity.notFound().build();
         }
     }
-// TODO modyfikowanie koszyka, usuwanie z koszyka, i potem w angularze do kazdej auckji zrobic obsloge
+
+    @PutMapping("{itemId}")
+    public ResponseEntity<Void> putItem(@PathVariable("login") String login, @PathVariable("itemId") int itemId, @RequestBody PutBasketItemRequest request) {
+        Optional<User> user = userService.findUser(login);
+        if (user.isPresent()) {
+            if (bean.findAll().size() >= itemId && itemId > 0) {
+                jgorny.portal.basket.bean.model.BasketItem original = bean.findAll().get(itemId - 1);
+                jgorny.portal.basket.bean.model.BasketItem modified = new jgorny.portal.basket.bean.model.BasketItem(request.getQuantity(), original.getAuction());
+                bean.save(modified);
+                return ResponseEntity.accepted().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("{itemId}")
+    public ResponseEntity<Void> deleteItem(@PathVariable("login") String login, @PathVariable("itemId") int itemId) {
+        Optional<User> user = userService.findUser(login);
+        if (user.isPresent()) {
+            if (bean.findAll().size() >= itemId && itemId > 0) {
+                bean.deleteById(itemId - 1);
+                return ResponseEntity.accepted().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
