23a6e4fac590cf1a3b48793a71b49f56c206c37a
jgorny <iceziro@wp.pl>
2020-10-23 17:38:25 +0200

    feat:added SecurityConfig

---
+++ src/main/java/jgorny/portal/user/authentication/SecurityConfig.java
@@ -0,0 +1,25 @@
+package jgorny.portal.user.authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().csrf().disable().authorizeRequests().antMatchers("/api/branches/**").hasRole("User");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("{noop}user").roles("User").and()
+                .withUser("admin").password("{noop}admin").roles("Admin", "User");
+    }
+
+}
