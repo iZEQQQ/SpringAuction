07cf662c1db1f7fa651ef8706ed47b59209aeade
jgorny <iceziro@wp.pl>
2020-10-23 17:48:59 +0200

    feat: added http authorization

--- src/main/java/jgorny/portal/user/authentication/SecurityConfig.java
+++ src/main/java/jgorny/portal/user/authentication/SecurityConfig.java
@@ -1,6 +1,7 @@
 package jgorny.portal.user.authentication;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,7 +12,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().csrf().disable().authorizeRequests().antMatchers("/api/branches/**").hasRole("User");
+        http.httpBasic().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/branches/**").hasRole("User")
+                .antMatchers(HttpMethod.PUT, "/api/branches/**").hasRole("Admin")
+                .antMatchers(HttpMethod.POST, "/api/branches/**").hasRole("Admin");
     }
 
     @Autowired
